3e4e2caa359ba6ce5c29be4e9e57196ca0583a0c
Dina <dinagiacobbi@gmail.com>
2024-03-01 13:15:03 -0900

    Adding route parameters and back button

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,30 +7,17 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
-
-  /*The new version waits for the Observable to emit the array of heroes, 
-  which could happen now or several minutes from now. 
-  The subscribe() method passes the emitted array to the callback, 
-  which sets the component's heroes property.
-
-    This asynchronous approach works when the HeroService 
-    requests heroes from a server. */
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
