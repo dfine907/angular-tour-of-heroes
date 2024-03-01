84a7f4b23bcb340d77183bb575a692bb8f74830a
Dina <dinagiacobbi@gmail.com>
2024-03-01 08:42:47 -0900

    Adding nav bar and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+
+/* The method is called forRoot() because you configure 
+the router at the application's root level. 
+The forRoot() method supplies the service providers 
+and directives needed for routing, and performs the 
+initial navigation based on the current browser URL. */
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
